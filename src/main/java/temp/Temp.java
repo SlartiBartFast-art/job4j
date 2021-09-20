75e54e1c10b0a5ed31a99ed037bdfaa706a90b34
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-20 15:38:00 +0500

    3.4. Временное хранение. git stash. [#309409] Несколько коммитов. #03

--- src/main/java/temp/Temp.java
+++ src/main/java/temp/Temp.java
@@ -3,5 +3,6 @@ package temp;
 public class Temp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Life is short don't be lazy");
     }
 }

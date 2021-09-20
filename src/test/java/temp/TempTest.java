1213122f28be59e36cb85d94a0bcec3f41b22082
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-20 15:30:17 +0500

    Несколько коммитов. #01

---
+++ src/test/java/temp/TempTest.java
@@ -0,0 +1,32 @@
+package temp;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import static org.junit.Assert.*;
+
+public class TempTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TempTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(TempTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}

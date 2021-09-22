fe2f4dda820cb8da9cacf15be4444fa088110275
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-22 13:28:25 +0500

    6. Удаление ветки и создание ветки из коммита [#125033 #206178] hotfix_3. Rewrite README

--- src/main/java/temp/Temp.java
+++ src/main/java/temp/Temp.java
@@ -2,7 +2,7 @@ package temp;
 
 public class Temp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Have a nice day!");
+        System.out.println("Hello World!!!");
+        System.out.println("Have a nice day!!!");
     }
 }
